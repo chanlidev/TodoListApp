15f9b8f93d100d3a220cb9609da721237cd3b9e3
Chan Li <chan.li.yyc@gmail.com>
2024-03-01 02:04:35 -0700

    Props and State Management

--- App.jsx
+++ App.jsx
@@ -4,15 +4,17 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import ToDoList from './components/ToDoList';
 import ToDoForm from './components/ToDoForm';
 
 function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
